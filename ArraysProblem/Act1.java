050e2a71cea35b45d8926fbec595d036d7362f87
KingNelx <tapiajonel606@gmail.com>
2022-06-17 06:29:43 +0800

    Exercise 1

---
+++ ArraysProblem/Act1.java
@@ -0,0 +1,17 @@
+package ArraysProblem;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Act1 {
+    public static void main (String[]args){
+        // Write a Java program to sort a numeric array and a string array.
+
+      int[]myNumbers = {350, 232, 456, 121, 879, 654, 478, 987, 428};
+      String[]myNames = {"Invoker", "Tinker", "Sniper", "Templar", "Earth shaker",
+            "Witch Doctor", "Shadow Fiend"};
+
+
+
+    }
+}
