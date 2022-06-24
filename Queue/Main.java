9671c9a3425d2ec8d19d7c7187d51bd8bbb1738a
KingNelx <tapiajonel606@gmail.com>
2022-06-25 05:32:43 +0800

    new update

--- Queue/Main.java
+++ Queue/Main.java
@@ -1,4 +1,24 @@
 package Queue;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
+    public static void main (String[]args){
+        // a queue is not a class (it is an abstract)
+        // offer (add) in the tail
+        // poll to remove in the head
+        // peek to view the top element
+        // Linear data first in first out
+        Queue<String> test = new LinkedList<>();
+
+        test.offer("Jonel");
+        test.offer("Tapia");
+        test.offer("KingNel");
+        test.offer("Invoking");
+
+        System.out.println(test.peek());
+//        test.poll();
+//        test.poll();
+        System.out.println(test);
+    }
 }
