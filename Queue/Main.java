e3fc9e4f4937562a2bc98f153d6c8c1ad0c7721e
KingNelx <tapiajonel606@gmail.com>
2022-06-25 05:38:07 +0800

    new update

--- Queue/Main.java
+++ Queue/Main.java
@@ -11,12 +11,16 @@ public class Main {
         // Linear data first in first out
         Queue<String> test = new LinkedList<>();
 
+        System.out.println(test.isEmpty());
+
         test.offer("Jonel");
         test.offer("Tapia");
         test.offer("KingNel");
         test.offer("Invoking");
 
+        System.out.println(test.size());
         System.out.println(test.peek());
+        System.out.println(test.contains("Tapia"));
 //        test.poll();
 //        test.poll();
         System.out.println(test);
