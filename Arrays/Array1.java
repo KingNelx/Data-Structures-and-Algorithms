095d74703bcb3d4913fd9e8a2a0d14160f2682b8
KingNelx <tapiajonel606@gmail.com>
2023-07-09 20:07:37 +0800

    single array

--- Arrays/Array1.java
+++ Arrays/Array1.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 public class Array1 {
-    public static void main(String[] args) throws Exception {
-        int[] myArray = new int[5];
-        myArray[0] = 1;
-        myArray[1] = 2;
-        myArray[2] = 3;
-        myArray[3] = 4;
-        myArray[4] = 5;
-        myArray[5] = 6; // index out of bounds since the size of the array is static (5)
-        for (int i : myArray) {
-            System.out.println(i);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print(" Input the size of the array: ");
+        int sizeN = input.nextInt();
+        int[] myArray = new int[sizeN];
+
+        // storing elements
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(" Input elements in the array: ");
+            myArray[i] = input.nextInt();
         }
+
+        // printing elements
+        System.out.println(" ELEMENTS: ");
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+
+        input.close();
     }
 }
